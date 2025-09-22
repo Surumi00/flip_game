1971e3add541bcf6a23e535a481b795b23d4f608
surumi K A <surumika44@gmail.com>
2025-09-22 14:49:27 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import { useState } from "react"
-import Game from './Game';
-import Home from './Home';
+import { useState } from "react";
+import Game from "./Game";
+import Home from "./Home";
 
 function App() {
- const [started,setstarted]=useState(false);
+  const [started, setstarted] = useState(false);
+  const [currentlevel, setcurrentlevel] = useState(1);
+  const [gamestatus, setgamestatus] = useState();
 
- return(<>
-       {started ? 
-          <Game/> :
-          <Home onstart={()=>setstarted(true)}/>}
- 
-       
-       </>
- );
+  function handlestart() {
+    setstarted(true);
+  }
 
+  function handlelevel() {
+    setcurrentlevel((prev) => prev + 1);
+    setgamestatus(undefined);
+  }
+  return (
+    <>
+      {!started ? (
+        <Home onstart={handlestart} />
+      ) : gamestatus === "win" ? (
+        <div className="win">
+          <h2>🎉 YOU WIN!!!! 🎉</h2>
+          <button onClick={handlelevel}>Next Level</button>
+        </div>
+      ) : gamestatus == "gameover" ? (
+        <div className="win">
+          <h2> Game Over!!!! </h2>
+          <button
+            onClick={() => {
+              setcurrentlevel(1);
+              setgamestatus(undefined);
+            }}
+          >
+            Try Again
+          </button>
+        </div>
+      ) : (
+        <Game
+          level={currentlevel}
+          onfinished={() => setgamestatus("win")}
+          ongameover={() => setgamestatus("gameover")}
+        ></Game>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,127 +1,136 @@
-import { useEffect, useState } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 import confetti from "canvas-confetti";
 
+function gameStarted({ level, onfinished,ongameover }) {
+  const [pieces, setpieces] = useState([]);
 
-function  gameStarted(){
-const[pieces,setpieces]=useState([]);
-   const [finished,setfinished]=useState(false);
+  let gameIcons = [];
+  const level1icons = ["🐢", "🐥", "🐸"];
+  const level2icons = ["🦊", "🦄", "🐧", "🐼"];
+  const level3icons = ["🌸", "✨", "😊", "💻", "📚", "🌍", "☕", "🎶"];
 
-  const gameIcons=["🌸","✨","😊","💻","📚","🌍","☕","🎶"]
+  function startgame() {
+    if (level === 1) gameIcons = [...level1icons];
+    else if (level === 2) gameIcons = [...level2icons];
+    else if (level === 3) gameIcons = [...level3icons];
+    else {
+      ongameover();
+      return;
+    }
 
-  function startgame(){
-    const duplicateicons=[...gameIcons,...gameIcons];
-    const newgameicon=[];
+    const duplicateicons = [...gameIcons, ...gameIcons];
+    const newgameicon = [];
 
-    while(newgameicon.length<gameIcons.length*2){
-    const randomindex=Math.floor(Math.random()*duplicateicons.length);
-    newgameicon.push({
-      emoji : duplicateicons[randomindex],
-      flipped : false,
-      solved : false,
-      position:newgameicon.length,
-    });
-    duplicateicons.splice(randomindex,1);
-  }
-  setpieces(newgameicon);
-}
-
-
-useEffect(()=>{
-  startgame();
-},[]);
-
- function handleclick(data){
-  const flippeddata=pieces.filter((data)=>data.flipped && !data.solved);
-  if (flippeddata.length===2)return;
-
-      const newpieces=pieces.map((piece)=>{
-        if(piece.position===data.position){
-           piece.flipped=!piece.flipped;
-        }
-        return piece;
+    while (newgameicon.length < gameIcons.length * 2) {
+      const randomindex = Math.floor(Math.random() * duplicateicons.length);
+      newgameicon.push({
+        emoji: duplicateicons[randomindex],
+        flipped: false,
+        solved: false,
+        position: newgameicon.length,
       });
-      setpieces(newpieces);
-
- }
-
- function gamelogicforflipped(){
-   const flippeddata=pieces.filter((data)=>data.flipped && !data.solved);
-
-   if(flippeddata.length===2){
-   setTimeout(()=>{
-      if(flippeddata[0].emoji === flippeddata[1].emoji){
-         setpieces(
-          pieces.map((piece)=>{
-            if( 
-              piece.position === flippeddata[0].position ||
-              piece.position === flippeddata[1].position
-            ){
-              piece.solved=true;
-            }
-            return piece;
-          }
-        ));
-      }
-   else{
-        setpieces(
-          pieces.map((piece)=>{
-            if( 
-              piece.position === flippeddata[0].position ||
-              piece.position === flippeddata[1].position
-            ){
-              piece.flipped=false;
-            }
-            return piece;
-          }
-        ));
-      }
-    },800);
-   }
- }
-
- const gamefinished = ()=>{
-      if(pieces.every((piece)=>piece.solved)){
-        setfinished(true);
-         confetti({
-      particleCount: 150,
-      spread: 70,
-      origin: { y: 0.6 }
-    });
-      }
- }
-
-
-
- useEffect(()=>{
-  gamelogicforflipped();
-
-  if(pieces.length>0){
-    gamefinished();
+      duplicateicons.splice(randomindex, 1);
+    }
+    setpieces(newgameicon);
   }
- },[pieces]);
+
+  useEffect(() => {
+    startgame();
+  }, [level]);
+
+  function handleclick(data) {
+    const flippeddata = pieces.filter((data) => data.flipped && !data.solved);
+    if (flippeddata.length === 2) return;
+
+    const newpieces = pieces.map((piece) => {
+      if (piece.position === data.position) {
+        piece.flipped = !piece.flipped;
+      }
+      return piece;
+    });
+    setpieces(newpieces);
+  }
+
+  function gamelogicforflipped() {
+    const flippeddata = pieces.filter((data) => data.flipped && !data.solved);
+
+    if (flippeddata.length === 2) {
+      setTimeout(() => {
+        if (flippeddata[0].emoji === flippeddata[1].emoji) {
+          setpieces(
+            pieces.map((piece) => {
+              if (
+                piece.position === flippeddata[0].position ||
+                piece.position === flippeddata[1].position
+              ) {
+                piece.solved = true;
+              }
+              return piece;
+            })
+          );
+        } else {
+          setpieces(
+            pieces.map((piece) => {
+              if (
+                piece.position === flippeddata[0].position ||
+                piece.position === flippeddata[1].position
+              ) {
+                piece.flipped = false;
+              }
+              return piece;
+            })
+          );
+        }
+      }, 800);
+    }
+  }
+
+  const gamefinished = () => {
+    if (pieces.every((piece) => piece.solved)) {
+      onfinished();
+      confetti({
+        particleCount: 150,
+        spread: 70,
+        origin: { y: 0.6 },
+      });
+    }
+  };
+
+  useEffect(() => {
+    gamelogicforflipped();
+
+    if (pieces.length > 0) {
+      gamefinished();
+    }
+  }, [pieces]);
 
   return (
     <>
-   
-     {finished && <div className="win" >🎉 YOU WIN!!!! 🎉 </div>
-     }
-       
-    <div className='container'>
-      {pieces.map((data,index)=>(
-           <div className={`flip-card ${data.flipped || data.solved ? "active" : ""} `} key={index} onClick={()=> handleclick(data)} >
-          <div className="flip-card-inner">
-          <div className="flip-card-front">?</div>
-          <div className="flip-card-back">
-          {data.emoji}
-    </div>
-  </div>
-</div>  
-  ))}
-   
-      
-    </div>      
+       <div>
+              { level===1 ? (<h3 className="levelsmention" >Level 1</h3>) 
+              : level===2 ? (<h3 className="levelsmention">level 2</h3>) 
+              : (<p></p>) }
+            </div>
+      <div className={ level===1 ?"container1" : level===2 ? "container2" : "container3"}>
+        
+        {pieces.map((data, index) => (
+          <div
+            className={`flip-card ${
+              data.flipped || data.solved ? "active" : ""
+            } `}
+            key={index}
+            onClick={() => handleclick(data)}
+          >  
+           
+            <div className="flip-card-inner">
+              <div className="flip-card-front">?</div>
+              <div className="flip-card-back">{data.emoji}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
-export default gameStarted
+export default gameStarted;
